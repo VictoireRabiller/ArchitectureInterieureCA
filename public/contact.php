24e897d2561b5667b3417047a0cc50d3389c4e02
VictoireRabiller <rabillervictoire@gmail.com>
2019-03-07 20:59:27 +0100

    container

--- public/contact.php
+++ public/contact.php
@@ -1,7 +1,18 @@
 
 <?php
 include 'bootstrap.php';
-
+require('../views/vendor/reCaptcha/autoload.php');
+if(isset($_POST['submit'])){ 
+  if(isset($_POST['gRecaptchaResponse'])){ 
+    $recaptcha = new \ReCaptcha\ReCaptcha('############');
+    $resp = $recaptcha->verify($_POST['$gRecaptchaResponse']);
+    if ($resp->isSuccess()) {
+        // Verified!
+    } else {
+        $errors = $resp->getErrorCodes();
+    }
+  }
+}
 /* D'abord on fixe la valeur par défaut des messages d'erreur et des variables des inputs */
 $erreurnom = $erreuremail = $erreurmessage = $messageenvoi = $civility = $nom = $prenom =$tel = $email = $message =  '';
 /* Ensuite on vérifie si le formulaire a été soumis et on valide les valeurs récupérées */
