455104352c61035dccdafbd31ff93fb805c06fb9
VictoireRabiller <rabillervictoire@gmail.com>
2019-03-24 14:23:29 +0100

    realisations particuliers 17 26 27

--- public/contact.php
+++ public/contact.php
@@ -47,63 +47,7 @@ if (!empty($_POST['submit'])) {
           $erreurmessageCA = '<br><span class="error">Vous n\'avez pas mis votre message</span><br>';
       }
     
-
-
-  // $mail = 'victoirecretal@hotmail.com';
-  // if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)){
-
-  //     $passage_ligne = "\r\n";
-
-  // }else{
-
-  //     $passage_ligne = "\n";
-
-  // }
-
-
-  //   /* Si tout est ok, on envoie le courriel */
   if ($valid) {
-
-  //   //=====Déclaration des messages au format texte et au format HTML.
-  //   $message_txt = "Salut à tous, voici un e-mail envoyé par un script PHP.";
-  //   $message_html = "<html><head></head><body><b>Salut à tous</b>, voici un e-mail envoyé par un <i>script PHP</i>.</body></html>";
-  //   //==========
-     
-  //   //=====Création de la boundary
-  //   $boundary = "-----=".md5(rand());
-  //   //==========
-     
-  //   //=====Définition du sujet.
-  //   $sujet = "Message de ton site web";
-  //   //=========
-     
-  //   //=====Création du header de l'e-mail.
-  //   $header = "From: \"Charlotte\"<victoirecretal@hotmail.com>".$passage_ligne;
-  //   $header.= "Reply-to: \"Charlotte\" <victoirecretal@hotmail.com>".$passage_ligne;
-  //   $header.= "MIME-Version: 1.0".$passage_ligne;
-  //   $header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
-  //   //==========
-     
-  //   //=====Création du message.
-  //   $message = $passage_ligne."--".$boundary.$passage_ligne;
-  //   //=====Ajout du message au format texte.
-  //   $message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
-  //   $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-  //   $message.= $passage_ligne.$message_txt.$passage_ligne;
-  //   //==========
-  //   $message.= $passage_ligne."--".$boundary.$passage_ligne;
-  //   //=====Ajout du message au format HTML
-  //   $message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
-  //   $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-  //   $message.= $passage_ligne.$message_html.$passage_ligne;
-  //   //==========
-  //   $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-  //   $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-  //   //==========
-     
-  //   //=====Envoi de l'e-mail.
-  //   mail($mail,$sujet,$message,$header);
-  //   //==========
 
 
     $messageenvoi =  'Votre message a bien été envoyé, merci !<br>';
